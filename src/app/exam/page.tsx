0a54078b9b656a2e68dd371ec5425241ab53f41a
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2024-07-21 16:30:01 +0200

    add index

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Index from "@/components";
 import Questions from "@/components/questions";
 import Result from "@/components/result";
 import { Button } from "@/components/ui/button";
@@ -8,18 +9,22 @@ export default function Exam() {
   const [isSubmit, setIsSubmit] = useState(false);
 
   return (
-    <div className="flex flex-col gap-20 items-center mt-20">
-      <Questions></Questions>
-      {isSubmit ? (
-        <Result></Result>
-      ) : (
-        <Button
-          onClick={() => setIsSubmit(true)}
-          className="hover:scale-125 transition duration-200 bg-neutral-950 mb-10"
-        >
-          Submit Answers!
-        </Button>
-      )}
-    </div>
+    <>
+      <Index></Index>
+
+      <div className="flex flex-col gap-20 items-center mt-20">
+        <Questions></Questions>
+        {isSubmit ? (
+          <Result></Result>
+        ) : (
+          <Button
+            onClick={() => setIsSubmit(true)}
+            className="hover:scale-125 transition duration-200 bg-neutral-950 mb-10"
+          >
+            Submit Answers!
+          </Button>
+        )}
+      </div>
+    </>
   );
 }
--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,3 +1,22 @@
+import { EXAMPLE } from "@/lib/constants";
+
 export default function Result() {
-  return <h2 className="text-white">Results</h2>;
+  return <h2 className="text-white">Result: {getResult()}</h2>;
+}
+
+function getResult() {
+  let result = 0;
+  Object.keys(EXAMPLE).forEach((key) => {
+    const answer = document.querySelector(`[data-question='${key}'] .active`);
+    if (answer) {
+      const index = EXAMPLE[key].answers.indexOf(answer.textContent);
+      if (index === EXAMPLE[key].correct) {
+        result += 1;
+        answer.classList.add("correct");
+      } else {
+        answer.classList.add("incorrect");
+      }
+    }
+  });
+  return result;
 }
