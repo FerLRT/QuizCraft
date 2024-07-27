5960943c88f47ff16062495bc30e4de6d79f6b6c
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-07-27 14:21:25 +0200

    Updated app name
    Co-Authored-By: Sergio García Muñoz <57625297+sergiogarciiam@users.noreply.github.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="text-white flex bg-neutral-950 p-5 justify-between">
       <a href="/" className="text-2xl font-bold">
-        QuizApp
+        QuizAI
       </a>
       <a href="/about">About Us</a>
     </header>
