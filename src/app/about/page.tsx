29965382f00d76654e8c5f0f8c24ada66b9ddce9
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-07-27 23:32:30 +0200

    Added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,110 @@
+import React from "react";
+import { TEAM_MEMBERS } from "../../lib/constants";
+import { MdiLinkedin } from "../../components/assets/linkedInIcon";
+import { MdiGithub } from "../../components/assets/githubIcon";
+
 export default function About() {
   return (
-    <main>
-      <h1 className="text-white text-2xl">About Us</h1>
+    <main className="bg-gray-900 text-white min-h-screen p-8">
+      <h1 className="text-4xl font-bold mb-8">About Us</h1>
+
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-4">Our Team</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {TEAM_MEMBERS.map((member) => (
+            <div
+              key={member.name}
+              className="bg-gray-800 p-6 rounded-lg flex flex-col"
+            >
+              <img
+                className="w-32 h-32 rounded-full mx-auto mb-4"
+                src={member.imageUrl.src}
+                alt={`${member.name}'s photo`}
+              />
+              <h3 className="text-2xl font-bold mb-2">{member.name}</h3>
+              <p className="text-xl mb-2">{member.role}</p>
+              <p>{member.description}</p>
+              <div className="flex justify-center space-x-4 mt-auto mb-0 ">
+                <a
+                  href={member.linkedIn}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <MdiLinkedin className="w-6 h-6" />
+                </a>
+                <a href={member.github} target="_blank">
+                  <MdiGithub className="w-6 h-6" />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-4">Who are we?</h2>
+        <p>
+          We are a team of passionate and innovative computer engineers, all
+          graduates from the University of Castilla-La Mancha. Our journey began
+          four years ago as university classmates, where we discovered our
+          shared enthusiasm for technology and continuous learning.
+        </p>
+        <p>
+          Over the past four years, we have participated in various hackathons,
+          both together and individually, including the HPe CDS Tech Challenge,
+          Telefónica's HackForGood and Entrepreneur. These experiences have not
+          only honed our teamwork skills but also enhanced our technical
+          abilities, inspiring us to join this hackathon.
+        </p>
+        <p>
+          Our diverse experiences and collaborative spirit drive us to tackle
+          challenges with creativity and dedication. We believe in the power of
+          technology to solve real-world problems and are committed to
+          leveraging our skills to create impactful solutions.
+        </p>
+      </section>
+
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-4">
+          Objectives and Purpose of the Application
+        </h2>
+        <p>
+          Our primary objective is to simplify the process of creating
+          educational tests from existing documents. We aim to make learning
+          more accessible and efficient for both educators and students by
+          providing a tool that automates the generation of test questions.
+        </p>
+        <p>This application serves two main purposes:</p>
+        <ul className="list-disc list-inside ml-4 space-y-2">
+          <li>
+            <strong>For Educators</strong>: It helps in automatically generating
+            questions from their existing documents, saving valuable time in
+            preparing assessments and enabling the creation of customized
+            educational materials.
+          </li>
+          <li>
+            <strong>For Students</strong>: It assists in generating practice
+            tests from their notes, improving their exam preparation and
+            reinforcing their understanding of study materials.
+          </li>
+        </ul>
+        <p>
+          This project stems from our own experiences as students. Having
+          recently completed our degrees in Computer Engineering and about to
+          start a Master's in Strategic IT Management, we understand the
+          challenges faced by both educators and students in the educational
+          sphere. Our application is designed to address these challenges,
+          offering a practical solution to enhance the learning and teaching
+          experience.
+        </p>
+        <p>
+          By leveraging our technical expertise and passion for education, we
+          are committed to developing a tool that not only saves time but also
+          enriches the educational process. We believe that through technology,
+          we can create a more interactive and effective learning environment
+          for everyone.
+        </p>
+      </section>
     </main>
   );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,7 @@
+import lucianImg from "../components/assets/lucian.jpg";
+import sergioImg from "../components/assets/sergio.jpg";
+import fernandoImg from "../components/assets/fernando.jpg";
+
 import { Exam } from "@/interfaces/questionInterface";
 
 export const EXAMPLE: Exam = {
@@ -27,3 +31,31 @@ export const EXAMPLE: Exam = {
     correct: 1,
   },
 };
+
+export const TEAM_MEMBERS = [
+  {
+    name: "Lucian Andrei Farcas",
+    role: "Computer Engineer",
+    description: "Description.",
+    imageUrl: lucianImg,
+    linkedIn: "https://www.linkedin.com/in/lucian-farcas/",
+    github: "https://github.com/Lucian-UCLM",
+  },
+  {
+    name: "Sergio García Muñoz",
+    role: "Computer Engineer",
+    description: "Description.",
+    imageUrl: sergioImg,
+    linkedIn: "https://www.linkedin.com/in/sergiogarciiam/",
+    github: "https://github.com/sergiogarciiam",
+  },
+  {
+    name: "Fernando Lorente Calvo",
+    role: "Computer Engineer",
+    description:
+      "I am passionate about technology and I am always looking for opportunities to learn and expand my knowledge. My goal is to contribute to innovative projects and continue to grow both professionally and personally, learning from every experience and challenge that comes my way.",
+    imageUrl: fernandoImg,
+    linkedIn: "https://www.linkedin.com/in/fernando-lorente-09aab0274/",
+    github: "https://github.com/FerLRT",
+  },
+];
