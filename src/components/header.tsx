8ac07bb19b946e791a359a94e387b1712a923aaf
Lucian <lucianandrei.farcas@alu.uclm.es>
2024-08-04 19:03:03 +0200

    app icon aupdated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
+import { AppLogoName } from "./assets/headbar_ico";
+
 export function Header() {
   return (
     <a
       href="/"
       className="text-4xl font-bold text-white mt-5 sm:ml-5 text-center sm:text-left"
     >
-      QuizCraft
+      <AppLogoName></AppLogoName>
     </a>
   );
 }
