bf70fdb57c9021c923a3e337929875c04234f073
Fernando <82894757+FerLRT@users.noreply.github.com>
2024-08-05 19:33:22 +0200

    Deleted unused variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,36 +2,6 @@ import lucianImg from "../components/assets/lucian.svg";
 import sergioImg from "../components/assets/sergio.svg";
 import fernandoImg from "../components/assets/fernando.svg";
 
-import { Exam } from "@/interfaces/questionInterface";
-
-export const EXAMPLE: Exam = {
-  1: {
-    question: "Question 1",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    correct: 1,
-  },
-  2: {
-    question: "Question 2",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    correct: 1,
-  },
-  3: {
-    question: "Question 3",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    correct: 1,
-  },
-  4: {
-    question: "Question 4",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    correct: 1,
-  },
-  5: {
-    question: "Question 5",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    correct: 1,
-  },
-};
-
 export const TEAM_MEMBERS = [
   {
     name: "Lucian Andrei Farcas",
